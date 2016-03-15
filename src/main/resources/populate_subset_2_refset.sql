04673432828ef45f98331cfa6428fa71bb66c8f1
Peter Groves Williams <petergwilliams@hotmail.com>
2016-03-15 16:57:30 +0000

    Correct subset names for US/GB English

--- src/main/resources/populate_subset_2_refset.sql
+++ src/main/resources/populate_subset_2_refset.sql
@@ -1,9 +1,9 @@
 -- Original SubsetIds are not known in RF2, so we need to populate those
 INSERT INTO RF2_subset2refset (originalSubsetId, SubsetName, RefsetId, fsn, location) 
-values (100032, 'GB English Language reference set', 900000000000508004, 'Great Britain English language reference set (foundation metadata concept)', 'Refset/Content/');
+values (100032, 'GB English Dialect Subset', 900000000000508004, 'Great Britain English language reference set (foundation metadata concept)', 'Refset/Content/');
 
 INSERT INTO RF2_subset2refset (originalSubsetId, SubsetName, RefsetId, fsn, location) 
-values (100033, 'US English Language reference set', 900000000000509007, 'United States of America English language reference set (foundation metadata concept)','Refset/Content/');
+values (100033, 'US English Dialect Subset', 900000000000509007, 'United States of America English language reference set (foundation metadata concept)','Refset/Content/');
 
 INSERT INTO RF2_subset2refset (originalSubsetId, SubsetName, RefsetId, fsn, location) 
 values (103030, 'Spanish Language Edition', 450828004, 'Spanish language reference set (foundation metadata concept)','Refset/Content/');
