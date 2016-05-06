8adb69691306d0dabad360ef0e380682420c6cb8
pgwilliams <petergwilliams@hotmail.com>
2016-05-06 12:47:02 +0100

    Increase memory for PC startup file

--- go.bat
+++ go.bat
@@ -1,5 +1,5 @@
-SET "memParams=-Xms2g -Xmx4g"
- rem SET "debugParams=-Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=8080"
- SET debugParams=
+SET "memParams=-Xms4g -Xmx8g"
+rem SET "debugParams=-Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=8080"
+SET debugParams=
 
 java -jar %memParams% %debugParams% target\RF2toRF1Converter.jar -u D:\ -v G:\incoming\SnomedCT_RF2Release_INT_20160131.zip
