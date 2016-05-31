d96bed3fe58b008c606e6759c2f9ef920a68e6bc
pgwilliams <petergwilliams@hotmail.com>
2016-05-31 12:15:09 +0100

    Added batch file option to change maximum memory available

--- go.bat
+++ go.bat
@@ -1,9 +1,13 @@
 @echo off
 
-SET "memParams=-Xms4g -Xmx8g"
+SET "memParams=-Xms4g -Xmx10g"
 SET debugParams=
 SET "rf2Archive=G:\incoming\SnomedCT_RF2Release_INT_20160131.zip"
 SET "secondDrive=D:\"
+
+SET newMemory=
+set /p newMemory="How much memory do you have available? [10g]: "
+IF NOT [%newMemory%]==[] SET "memParams=-Xms4g -Xmx%newMemory%"
 
 SET driveParam=
 set /p driveAvailable="Do you have a 2nd drive (%secondDrive%) Y/N: "
@@ -11,7 +15,7 @@ IF /I "%driveAvailable%"=="Y" SET "driveParam=-u %secondDrive%"
 
 SET newLocation=
 SET /p newLocation="Where is the RF2 Archive? [%rf2Archive%]: "
-IF /I NOT [%newLocation%]==[] SET "rf2Archive=%newLocation%"
+IF NOT [%newLocation%]==[] SET "rf2Archive=%newLocation%"
 
 FOR %%a IN (%*) DO (
   IF /I "%%a"=="-d" SET "debugParams=-Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=8080"
